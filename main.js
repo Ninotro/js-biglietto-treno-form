1ea44d6491ddf55be5157f7d75f008520b8440c3
Ninotro <ninotroia96@gmail.com>
2023-04-28 16:09:56 +0200

    start

---
+++ main.js
@@ -0,0 +1,18 @@
+// definisco le funzioni
+let distanza,userEta,bigliettoPrezzo,discountOver,discountYoung,priceOver,priceYoung;
+const prezzoPerKilometro= 0.21;
+
+// definisco la funzione per selezionare il valore on click
+
+// Valore distanza
+
+distanza = document.getElementById("form1").addEventListener("click", myFunction);
+
+
+
+
+function myFunction() {
+    document.getElementById("form1").value
+}
+
+console.log(distanza)
